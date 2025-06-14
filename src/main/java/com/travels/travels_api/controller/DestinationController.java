9b63a5976443c286a2eb5ec3a680628d6f3788fd
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-06-14 13:00:12 -0300

    Ajustes após testes

--- src/main/java/com/travels/travels_api/controller/DestinationController.java
+++ src/main/java/com/travels/travels_api/controller/DestinationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,7 +43,7 @@ public class DestinationController {
     @PostMapping
     public ResponseEntity<Response<Destination>> post(@RequestBody Destination destination) {
         Destination result = destinationService.create(destination);
-        return ResponseEntity.ok(new Response<Destination>(null, result));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new Response<Destination>(null, result));
     }
 
     @PatchMapping("/{id}/rate")
