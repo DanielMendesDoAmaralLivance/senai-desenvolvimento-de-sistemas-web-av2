2b2b9c52f01cb82e3ed534f0e141912967a8a292
DanielMendesDoAmaralLivance <daniel.amaral@livance.com.br>
2025-06-14 10:25:26 -0300

    Initial commit

---
+++ src/test/java/com/travels/travels_api/TravelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travels.travels_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
